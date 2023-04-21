72b033fc99763de58542ef15f3aebc1845730944
hjkrannig <hj@krannig.com>
2023-04-21 12:58:46 +0200

    index.css and reducer.js changed

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,7 @@
 import { TOGGLE_THEME, SEARCH_VALUE, PAGE } from './actions'
 
 export const reducer = (state, action) => {
+  // TOGGLE_THEME
   if (action.type === TOGGLE_THEME) {
     const { isDarkTheme } = state
     const body = document.querySelector('body')
@@ -8,13 +9,15 @@ export const reducer = (state, action) => {
     localStorage.setItem('darkTheme', !isDarkTheme)
     return { ...state, isDarkTheme: !isDarkTheme }
   }
+  // SEARCH_VALUE
   if (action.type === SEARCH_VALUE) {
     const search = action.payload.searchValue
     return { ...state, searchValue: search }
   }
+  // PAGE
   if (action.type === PAGE) {
     const page = action.payload.page
-    return { ...state, page }
+    if (page) return { ...state, page }
   }
   return { ...state }
 }
